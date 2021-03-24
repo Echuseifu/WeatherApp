054fe8c7d8254dbd6412ded35cedf3fb7ed1fb05
Echuseifu <79155072+Echuseifu@users.noreply.github.com>
2021-03-23 22:30:45 -0400

    Add files via upload

---
+++ src/Components/Home.js
@@ -0,0 +1,20 @@
+import Background from '../CSS/Background.css';
+
+
+ import Footer from './Footer.js';
+
+function Home() {
+  return (
+    <div className="background">
+      <h1>Home</h1>
+      <div>
+        <br /><br /><br /><br /><br /> <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+        <div className="foot">
+          <Footer />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
